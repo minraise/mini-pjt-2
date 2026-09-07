9b023558bc154c04859fd1db7ef01db431e866ee
minraise <raisethemin@gmail.com>
2026-09-07 14:30:37 +0900

    Day 1 — 기획 & 데이터 확보

---
+++ temp.py
@@ -0,0 +1,8 @@
+# API 응답
+import pandas as pd 
+kik = pd.read_excel('data/raw/KIKcd_H.20260720.xlsx')
+
+sgg = kik[(kik['시도명']=='서울특별시') & (kik['시군구명'].notna()) & (kik['읍면동명'].isna())]
+
+print(len(sgg))
+print(sgg[['행정동코드', '시군구명']].to_string())
